6f74822591a5a28d62202fd37d67285ce94747d7
webwithrathor <devrajrathor143@gmail.com>
2025-03-30 17:11:09 +0530

    updated cors

--- src/index.js
+++ src/index.js
@@ -2,24 +2,39 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
-import recipeRoutes from './routes/recipe.Routes.js'
+import recipeRoutes from "./routes/recipe.Routes.js";
 import { config } from "./config/config.js";
 import cookieParser from "cookie-parser";
 import { isAuthenticated } from "./middlewares/auth.middleware.js";
+
 const app = express();
 
-app.use(cors({origin:config.CLIENT_URL, credentials: true}));
+const allowedOrigins = [
+  "https://recipie-gen-frontend-9eqtd6uor.vercel.app",
+  "http://localhost:5173" // Allow localhost for development
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.some(allowed => allowed === origin.replace(/\/$/, ""))) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/recipe',isAuthenticated, recipeRoutes);
+app.use('/api/recipe', isAuthenticated, recipeRoutes);
 app.use("/api/auth", authRoutes);
 
 app.use("*", (req, res) => {
   res.status(404).json({ message: "Page not found" });
 });
-
 
 export default app;
